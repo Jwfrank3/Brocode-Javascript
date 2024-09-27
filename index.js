8661de46cf763f9af4018c8b0e77c47b3985504e
Jwfrank3 <jwfrank3@protonmail.com>
2024-09-26 23:32:56 -0500

    updated javascript.  Was studying on brocode up to 33:47 for Accept User Input.  Finishe dlearning Arithmetic operators.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-will update later
+console.log(`Hello World!`);
+console.log("I like Pizza!");
+console.log("I like Ice Cream");
+//window.alert(`Wee Woo`);
+//window.alert(`wee woo`);
+// alerts are so fricking annoying.  but at some point i'm supposed to use them.
+
+/* css syntax for comments appear to
+allow broken
+lines   
+            to create comments
+the // just allows one line not multiple broken lines for comments. */
+
+let students = 30;
+//students *= 3;
+
+//students /= 2
+//students **= 2;
+
+let result = 6 / 2 ** (2 + 5);
+
+console.log(result);
