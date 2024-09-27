022bb97258ec2c31141d0cf7d3b2f79c4a38a819
Jwfrank3 <jwfrank3@protonmail.com>
2024-09-27 10:55:47 -0500

    finished brocode up to the point of making a counter program.

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-console.log(`Hello World!`);
-console.log("I like Pizza!");
-console.log("I like Ice Cream");
-//window.alert(`Wee Woo`);
-//window.alert(`wee woo`);
-// alerts are so fricking annoying.  but at some point i'm supposed to use them.
+const PI = 3.14159;
+let radius;
+let circumeference;
+radius = Number(radius);
 
-/* css syntax for comments appear to
-allow broken
-lines   
-            to create comments
-the // just allows one line not multiple broken lines for comments. */
-
-let students = 30;
-//students *= 3;
-
-//students /= 2
-//students **= 2;
-
-let result = 6 / 2 ** (2 + 5);
-
-console.log(result);
+document.getElementById("mySubmit").onclick = function () {
+  radius = document.getElementById("myText").value;
+  radius = Number(radius);
+  circumeference = 2 * PI * radius;
+  document.getElementById("myH3").textContent = circumeference + "cm";
+};
